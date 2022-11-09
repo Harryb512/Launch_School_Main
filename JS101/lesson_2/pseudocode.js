89dc1d6f7ac7e7d74e039f4c783b14ba0e61cd24
Harryb512 <87959747+Harryb512@users.noreply.github.com>
2022-11-09 18:17:46 -0500

    JS101 Calculator and pseudocode

---
+++ JS101/lesson_2/pseudocode.js
@@ -0,0 +1,64 @@
+/* 
+1.  A function that returns the sum of two numbers
+ Start
+
+  declare function that accepts two arguments
+  set variable 
+  add both arguments together and set variable = sum
+  return variable
+
+  End
+    
+2. A function that takes an array of strings and returns a string that is all those strings concatenated together
+
+Start 
+ 
+  declare function that accepts one argument
+  set variable equal to empty string
+  set iterator = 0
+  while iterator < length of array
+    variable = variable + argument[i]
+    return variable
+
+    End
+
+
+3. A method that takes an array of integers and returns a new array with evey other element from the original array starting with the first element
+
+Start
+
+  declare function that accepts array of integers
+  set variable = empty array
+  push arguement[0] to variable
+  set iterator = 1
+  while interator < length of argument
+    if index of arguement[i] is an even number, push to variable
+    return variable
+
+    End
+  
+4. A function that determines the index of the 3rd occurrence of a given character in a string.
+
+Start
+
+  Declare a function that accepts a string as an arguement
+  set counter = 1
+  while parsing through string
+    if string[i] === string[i]
+    counter = counter +1
+    if counter >= 3 
+    return index of 3rd occurence 
+
+    End
+5. A function that takes two arrays of numbers and returns the result of merging the arrays.
+
+Start
+
+  Declare a function that accepts two arrays of number
+  set variable = empty array
+  set iterator = 0
+  while iterator < length of array
+  variable = array[i++]
+  return variable 
+
+  End
