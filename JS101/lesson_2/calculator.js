84421c82a77b20bdedae600eae67db3785700a77
Harryb512 <87959747+Harryb512@users.noreply.github.com>
2022-11-12 17:21:16 -0500

    remade calculator from scratch

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -5,50 +5,30 @@
 // Print the result to the terminal
 
 const readline = require('readline-sync');
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
-function invalidNumber(number) {
-  return number.trimStart() === '' || Number.isNaN(Number(number));
-}
+prompt('Welcome to the Calculator');
 
-prompt('Welcome to the Calculator!');
-
-prompt('What is the first number?');
+prompt('Please enter a number');
 let number1 = readline.question();
 
-while (invalidNumber(number1)) {
-  prompt('Please enter a valid number');
-  number1 = readline.question();
-}
-prompt('What is the second number?');
+prompt('Please enter a second number');
 let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt('Please enter a valid number');
-  number2 = readline.question();
-}
 
-prompt('What operation would you like to perform?\n1)Add 2) Subtract 3) Divide 4) Multiply');
+prompt('Please choose the operation you like to perform\n1)Add 2)Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4');
-  operation = readline.question();
-}
 let output;
 switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
+  case '1': output = Number(number1) + Number(number2);
     break;
-  case '2':
-    output = Number(number1) - Number(number2);
+  case '2': output = Number(number1) - Number(number2);
     break;
-  case '3':
-    output = Number(number1) / Number(number2);
+  case '3': output = Number(number1) * Number(number2);
     break;
-  case '4':
-    output = Number(number1) * Number(number2);
+  case '4': output = Number(number1) / Number(number2);
     break;
 }
-
 console.log(`The result is : ${output}`);
