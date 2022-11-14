d24352540f0e2f735af9459b244921fb269c55a9
Harryb512 <87959747+Harryb512@users.noreply.github.com>
2022-11-14 04:32:42 -0500

    refactored calculator.js

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -5,7 +5,8 @@
 // Print the result to the terminal
 
 const readline = require('readline-sync');
-
+const MESSAGES = require('./calculator_messgaes.json');
+let continueMath;
 function prompt(message) {
   console.log(`=> ${message}`);
 }
@@ -14,38 +15,52 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to the Calculator');
+prompt(MESSAGES['welcome']);
 
-prompt('Please enter a number');
-let number1 = readline.question();
-while (invalidNumber(number1)) {
-  prompt('Please enter a valid number');
-  number1 = readline.question();
-}
-prompt('Please enter a second number');
-let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt('Please enter a valid number');
-  number2 = readline.question();
-}
+do {
+  prompt('Please enter a number');
+  let number1 = readline.question();
 
-prompt('Please choose the operation you like to perform\n1)Add 2)Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+  while (invalidNumber(number1)) {
+    prompt('Please enter a valid number');
+    number1 = readline.question();
+  }
+  prompt('Please enter a second number');
+  let number2 = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Please choose either 1, 2, 3, or 4');
-  operation = readline.question();
-}
+  while (invalidNumber(number2)) {
+    prompt('Please enter a valid number');
+    number2 = readline.question();
+  }
 
-let output;
-switch (operation) {
-  case '1': output = Number(number1) + Number(number2);
-    break;
-  case '2': output = Number(number1) - Number(number2);
-    break;
-  case '3': output = Number(number1) * Number(number2);
-    break;
-  case '4': output = Number(number1) / Number(number2);
-    break;
-}
-console.log(`The result is : ${output}`);
+  prompt('Please choose the operation you like to perform\n1)Add 2)Subtract 3) Multiply 4) Divide');
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Please choose either 1, 2, 3, or 4');
+    operation = readline.question();
+  }
+
+
+  let output;
+  switch (operation) {
+    case '1': output = Number(number1) + Number(number2);
+      break;
+    case '2': output = Number(number1) - Number(number2);
+      break;
+    case '3': output = Number(number1) * Number(number2);
+      break;
+    case '4': output = Number(number1) / Number(number2);
+      break;
+  }
+  console.log(`The result is : ${output}`);
+
+  prompt('Would you like to make another calculation? Please enter y for yes or n for no');
+  continueMath = readline.question();
+
+  while (!['y', 'n'].includes(continueMath)) {
+    prompt('Please enter either y or n');
+    continueMath = readline.question();
+  }
+} while (continueMath === 'y')
+
